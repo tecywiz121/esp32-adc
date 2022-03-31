b17abf48a7ed6b56540da32bb1e320ef9f4ffcc2
Sam Wilson <tecywiz121@hotmail.com>
2022-03-31 14:59:34 -0400

    Show weird behavior

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use embedded_hal::adc::nb::OneShot;
+use embedded_hal_0_2_7::adc::OneShot as OneShot0_2_7;
+
+use embedded_hal_1_0_0::adc::nb::OneShot as OneShot1_0_0;
 
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-use esp_idf_hal::prelude::*;
 use esp_idf_hal::adc::{self, PoweredAdc};
 use esp_idf_hal::gpio::Pin;
+use esp_idf_hal::prelude::*;
 
 fn main() {
     esp_idf_sys::link_patches();
@@ -14,13 +16,18 @@ fn main() {
     let config = adc::config::Config::new().calibration(true);
     let mut adc = PoweredAdc::new(p.adc2, config).unwrap();
 
-    unsafe {
-        esp_idf_sys::gpio_set_pull_mode(pin.pin(), esp_idf_sys::gpio_pull_mode_t_GPIO_PULLUP_ONLY);
-    }
+    esp_idf_sys::esp!(unsafe {
+        esp_idf_sys::gpio_set_pull_mode(pin.pin(), esp_idf_sys::gpio_pull_mode_t_GPIO_PULLUP_ONLY)
+    })
+    .unwrap();
 
     loop {
-        let value = nb::block!(adc.read(&mut pin)).unwrap();
-        println!("Value: {}", value);
+        let value = nb::block!(OneShot0_2_7::read(&mut adc, &mut pin)).unwrap();
+        println!("Value 0.2.7: {}", value);
+
+        let value = nb::block!(OneShot1_0_0::read(&mut adc, &mut pin)).unwrap();
+        println!("Value 1.0.0: {}", value);
+
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
