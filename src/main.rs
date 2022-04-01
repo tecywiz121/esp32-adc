cc5bfe05c8646b5c88789397a31740a589e0dfde
Sam Wilson <tecywiz121@hotmail.com>
2022-03-31 22:07:10 -0400

    print what I think is the channel

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use embedded_hal_0_2_7::adc::OneShot as OneShot0_2_7;
+use embedded_hal_0_2_7::adc::{OneShot as OneShot0_2_7, Channel as Channel0_2_7};
 
-use embedded_hal_1_0_0::adc::nb::OneShot as OneShot1_0_0;
+use embedded_hal_1_0_0::adc::nb::{OneShot as OneShot1_0_0, Channel as Channel1_0_0};
 
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 use esp_idf_hal::adc::{self, PoweredAdc};
 use esp_idf_hal::gpio::Pin;
 use esp_idf_hal::prelude::*;
+
+fn channel0_2_7<T, U>(_: &T) -> T::ID where T: Channel0_2_7<U> {
+    T::channel()
+}
 
 fn main() {
     esp_idf_sys::link_patches();
@@ -23,10 +27,12 @@ fn main() {
 
     loop {
         let value = nb::block!(OneShot0_2_7::read(&mut adc, &mut pin)).unwrap();
-        println!("Value 0.2.7: {}", value);
+        println!("Channel 0.2.7: {}", channel0_2_7(&pin));
+        println!("Value   0.2.7: {}", value);
 
         let value = nb::block!(OneShot1_0_0::read(&mut adc, &mut pin)).unwrap();
-        println!("Value 1.0.0: {}", value);
+        println!("Channel 1.0.0: {}", Channel1_0_0::channel(&pin));
+        println!("Value   1.0.0: {}", value);
 
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
